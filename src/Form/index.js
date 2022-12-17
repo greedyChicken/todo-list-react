2ddd752f47c9f354a1430d4e597313165029eba7
Jan Hickiewicz <janhi@tutanota.com>
2022-12-17 23:40:23 +0100

    add addNewTask funcitonality

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,12 +1,25 @@
+import { useState } from "react";
 import "./style.css";
 
-const Form = () => {
+const Form = ({ addNewTask }) => {
+  const [newTaskContent, setNewTaskContent] = useState("");
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+    addNewTask(newTaskContent);
+    setNewTaskContent("");
+  };
+
   return (
-    <form className="form">
+    <form className="form" onSubmit={onFormSubmit}>
       <input
         type="text"
+        value={newTaskContent}
         className="form__input"
         placeholder="Co jest do zrobienia?"
+        onChange={({ target }) => {
+          setNewTaskContent(target.value);
+        }}
       />
       <button className="form__button">Dodaj zadanie</button>
     </form>
